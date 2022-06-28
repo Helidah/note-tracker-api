a6852fb17607c198397713197e8f6b890cc02636
George-Okumu <gokumu12@gmail.com>
2022-06-28 16:25:43 +0300

    serialized note model

--- Gemfile
+++ Gemfile
@@ -31,4 +31,7 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+#serializer
+gem "active_model_serializers", "~> 0.10.12"
+
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   
     has_secure_password
   
-    validates :bio, presence: true
+    validates :bio, :email, presence: true
     validates :username, :email,  presence: true, uniqueness: true
 end
