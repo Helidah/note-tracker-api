1302db0f5188e43d1d46075b653947c9131e6d73
George-Okumu <gokumu12@gmail.com>
2022-06-28 10:12:06 +0300

    Implemented MVC pattern

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,16 @@
 class NotesController < ApplicationController
+    def index
+        render json: Note.all
+      end
+    
+      def create
+        notes = @current_user.notes.create!(note_params)
+        render json: notes, status: :created
+      end
+    
+      private
+    
+      def note_params
+        params.permit(:title, :content)
+      end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,6 @@
 class Note < ApplicationRecord
+    belongs_to :user
+
+    validates :title, :content, presence: true
+    validates :content, length: { minimum: 50 }
 end
